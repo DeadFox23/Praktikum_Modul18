58cb66f37266425ec7b0c052663699962d1a9cfd
DeadFox23 <127043257+DeadFox23@users.noreply.github.com>
2025-05-12 12:49:08 +0200

    import

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		DataImport();
+	}
+
+	private static void DataImport()
+	{
+		Console.Write("C:\\datenbanken\\e.txt");
+		string filePath = Console.ReadLine();
+
+		if (File.Exists(filePath))
+		{
+			string fileContent = File.ReadAllText(filePath);
+			Console.WriteLine("\nFile content:\n");
+			Console.WriteLine(fileContent);
+		}
+		else
+		{
+			Console.WriteLine("File not found.");
+		}
+
+		Console.WriteLine("\nPress any key to exit...");
+		Console.ReadKey();
+	}
+}
